2980d49e9bb3eb6a66ae26028777a8c8a440a818
ralomach <ralomach@gmail.com>
2018-09-10 20:38:07 -0300

    changing to floats

--- ex_03_01.py
+++ ex_03_01.py
@@ -3,9 +3,9 @@ h = float(hrs)
 rt = input("Enter rate:")
 r = float(rt)
 
-if h <= 40 :
+if h <= 40.0 :
     pay = h * r
 else :
-    pay = (40 * r) + ((h - 40) * r * 1.5)
+    pay = (40.0 * r) + ((h - 40.0) * r * 1.5)
 
 print(pay)
