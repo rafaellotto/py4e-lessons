36a629611cd27e4ad4d12b5a603763e826a56fba
ralomach <ralomach@gmail.com>
2018-09-11 15:56:04 -0300

    exercise 8.5

---
+++ ex_08_05.py
@@ -0,0 +1,8 @@
+file = open("mbox-short.txt")
+count = 0
+for line in file :
+    if not line.startswith("From ") : continue
+    print(line.split()[1])
+    count = count + 1
+
+print("There were %d lines in the file with From as the first word" % count)
