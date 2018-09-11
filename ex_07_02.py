f8b5f776814e2e678f9f294bffd36a9fc8ea99a4
ralomach <ralomach@gmail.com>
2018-09-11 14:34:28 -0300

    exercise 7.2

---
+++ ex_07_02.py
@@ -0,0 +1,19 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+numbers = []
+
+for line in fh :
+    line = line.rstrip()
+    if not line.startswith("X-DSPAM-Confidence: ") : continue
+    pos = line.find(" ")
+    num = float(line[pos:])
+    numbers.append(num)
+
+total = 0
+
+for number in numbers :
+    total = total + number
+
+avg = total / len(numbers)
+
+print("Average spam confidence:", avg)
