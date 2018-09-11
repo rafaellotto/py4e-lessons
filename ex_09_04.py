37502896ee10a578c2293ede1ed71f30341a5c4d
ralomach <ralomach@gmail.com>
2018-09-11 17:01:10 -0300

    exercise 9.4

---
+++ ex_09_04.py
@@ -0,0 +1,19 @@
+file = open("mbox-short.txt")
+
+senders = {}
+for line in file :
+    if not line.startswith("From ") : continue
+    sender = line.split()[1]
+    if sender not in senders :
+        senders[sender] = 1
+    else :
+        senders[sender] += 1
+
+max = 0
+user = ""
+for sender in senders :
+    if senders[sender] > max :
+        max = senders[sender]
+        user = sender
+
+print(user, max)
