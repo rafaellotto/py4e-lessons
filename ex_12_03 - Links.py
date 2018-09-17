8dd86e5b194940297f28b3e9c68371f714e06026
ralomach <ralomach@gmail.com>
2018-09-17 16:22:17 -0300

    exercise 12 - Following Links

--- ex_12_03 - Links.py
+++ ex_12_03 - Links.py
@@ -1,6 +1,7 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import re
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -22,24 +23,43 @@ while type(count) is not int :
         count = abs(count)
         print("You want to repeat %d time(s) the position %d." % (count,position) )
     except ValueError:
-        print("%s is not an integer.\n" % count)
+        print("%s is not an integer.\nTry again: " % count)
 
 if user_input == 1 :
     url = sample
 else :
     url = problem
 
+names = []
+links = []
+
+first_name = re.findall("by_(\S+).html", url)
+names.append(first_name[0])
+
 def spider(url) :
+    print("Retrieving:", url)
     urls = []
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
-
     # Retrieve all of the anchor tags
     tags = soup('a')
     for tag in tags:
         urls.append(tag.get('href', None))
-    count -= 1
-    print(count)
+    # Get position url chosen by the user
+    url = urls[position - 1]
+    links.append(url)
+    # Add name to names
+    name = re.findall("by_(\S+).html", url)
+    names.append(name[0])
 
 while count > 0 :
-    spider(url)
+    if len(links) != 0 :
+        index = len(links) - 1
+        newurl = links[index]
+        spider(newurl)
+    else :
+        spider(url)
+    count -= 1
+
+print("Retrieving:", links[(len(links) - 1)])
+print("Last name:", names[(len(names) - 1)])
