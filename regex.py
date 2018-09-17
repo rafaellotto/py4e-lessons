9eb10b24c8ac0ae19cf9e6ea4003dcb200f2a7e0
ralomach <ralomach@gmail.com>
2018-09-17 12:21:14 -0300

    exercise 11 - Regular Expressions

---
+++ regex.py
@@ -0,0 +1,16 @@
+import re
+
+file = open("regex_sum_135221.txt")
+
+numbers = list()
+
+for line in file :
+	search = re.findall("\d+", line)
+	if len(search) > 0 :
+		for num in search :
+			numbers.append(int(num))
+
+sum = sum(numbers)
+count = len(numbers)
+	
+print(sum)
